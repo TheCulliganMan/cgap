8ac6bb7de1a450d28de9c458c076198b60871c13
Ryan Culligan <rrculligan@gmail.com>
2016-08-19 15:13:46 -0500

    modify setup names

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='extract_autosome',
+    name='cgap_v2',
     version='0.0.1',
-    description='extract_autosome package for Python-Guide.org',
+    description='cgap_v2 package for Python-Guide.org',
     long_description=readme,
     author='Ryan Culligan',
     author_email='rrculligan@gmail.com',
-    url='https://github.com/TheCulliganMan/extract_autosome',
+    url='https://github.com/TheCulliganMan/cgap_v2',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
