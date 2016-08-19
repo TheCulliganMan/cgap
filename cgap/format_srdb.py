2151ad2962a1d8b4c9180a11b783ca601cbeb1fb
Ryan Culligan <rrculligan@gmail.com>
2016-08-19 08:59:02 -0500

    new formatting method

--- cgap/format_srdb.py
+++ cgap/format_srdb.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def format_srdb_cmds(file_name):
-    cmd = ["perl", "bin/formatSRDB.pl", file_name]
-    return cmd
+import subprocess as sp
 
-def run_format_cmd(fastq):
-    format_srdb_cmds(fastq)
-    return sp.call(cmd)
+from Bio import SeqIO
+
+def fastq_to_fasta(fastq_path):
+    with open(fastq_path) as input_handle:
+        for record in SeqIO.parse(input_handle, 'fastq'):
+            yield record.format('fasta')
+
+
+def run_format_cmd(fastq_path):
+    format_cmd = ['formatdb',
+                  '-i', 'stdin',
+                  '-n', fastq_path,
+                  '-p', 'F']
+
+    p1 = Popen(
+        format_cmd,
+        stdin=subprocess.PIPE
+    )
+
+    fasta_generator = fastq_to_fasta(fastq_path)
+
+    for fasta_record in fasta_generator:
+        p1.stdin.write(fasta_record)
+
+    return True
