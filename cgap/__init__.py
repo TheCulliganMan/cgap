3e9da1e9d0595496d87cf28a0ccdb462ef1cdd78
Ryan Culligan <rrculligan@gmail.com>
2016-08-19 08:59:25 -0500

    streamline hits collection

--- cgap/__init__.py
+++ cgap/__init__.py
@@ -3,6 +3,5 @@
 
 from format_srdb import run_format_cmd
 from get_blast import run_blast
-from make_hits import get_hits
 from collect_hits import collect_hits
 from build_consensus import pipe_consensus
