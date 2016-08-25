f8e5423d6361d45fe65345483afffb84956f054d
Ryan Culligan <rrculligan@gmail.com>
2016-08-25 12:02:39 -0500

    fix extensions, change name merge

--- cgap/make_paths.py
+++ cgap/make_paths.py
@@ -38,43 +38,43 @@ def get_path(fasta_ref, fastq, directory, ext):
 
 def get_fastq_file_path(fasta_ref, fastq):
     ''' builds the fastq file path '''
-    path = get_path(fasta_ref, fastq, FASTQ_PATH, ".fastq")
+    path = get_path(fasta_ref, fastq, FASTQ_PATH, "fastq")
     return path
 
 
 def get_blast_file_path(fasta_ref, fastq):
     ''' builds the blast file path '''
-    path = get_path(fasta_ref, fastq, BLAST_PATH, ".blast")
+    path = get_path(fasta_ref, fastq, BLAST_PATH, "blast")
     return path
 
 
 def get_bam_file_working_path(fasta_ref, fastq):
     ''' builds a duplicated bam working file '''
-    path = get_path(fasta_ref, fastq, BAM_PATH, ".working.bam")
+    path = get_path(fasta_ref, fastq, BAM_PATH, "working.bam")
     return path
 
 
 def get_bam_file_path(fasta_ref, fastq):
     ''' gets the finalized bamfile path '''
-    path = get_path(fasta_ref, fastq, BAM_PATH, ".bam")
+    path = get_path(fasta_ref, fastq, BAM_PATH, "bam")
     return path
 
 
 def get_vcf_file_path(fasta_ref, fastq):
     ''' gets the snp vcf path '''
-    path = get_path(fasta_ref, fastq, VCF_PATH, ".vcf.gz")
+    path = get_path(fasta_ref, fastq, VCF_PATH, "vcf.gz")
     return path
 
 
 def get_depth_file_path(fasta_ref, fastq):
     ''' builds a depth file path for masking '''
-    path = get_path(fasta_ref, fastq, DEPTH_PATH, ".tsv")
+    path = get_path(fasta_ref, fastq, DEPTH_PATH, "tsv")
     return path
 
 
 def get_cns_file_path(fasta_ref, fastq):
     ''' builds a consensus file path '''
-    path = get_path(fasta_ref, fastq, CONSENSUS_FILES, ".fa")
+    path = get_path(fasta_ref, fastq, CONSENSUS_FILES, "fa")
     return path
 
 
@@ -88,10 +88,13 @@ def get_fastq_pair_name(fw_fq, rv_fq):
     fq1 = os.path.basename(fw_fq).rsplit(".")[0]
     fq2 = os.path.basename(rv_fq).rsplit(".")[0]
     new_name = ""
-    for i, j in zip(fq1, fq2):
+    for num, items in enumerate(zip(fq1, fq2)):
+        i, j = items
         if i == j:
             new_name += i
         else:
+            if not num:
+                new_name = "{}.{}".format(fq1, fq2)
             new_name += "."
             break
     return new_name
