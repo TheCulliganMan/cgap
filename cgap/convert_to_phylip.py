a799cfedf391b80a48de15fd997158d7819f7cc4
Ryan Culligan <rrculligan@gmail.com>
2016-08-25 13:36:55 -0500

    add documentation

--- cgap/convert_to_phylip.py
+++ cgap/convert_to_phylip.py
@@ -7,13 +7,15 @@ from .make_paths import get_phylip_file_path
 from .make_paths import get_fastq_pair_name
 
 def get_cns_files_for_fasta(fasta, fw_fqs, rv_fqs):
+    ''' yeilds cns information for a given fasta '''
     for fw_fq, rv_fq in zip(fw_fqs, rv_fqs):
         fq_pair = get_fastq_pair_name(fw_fq, rv_fq)
         cns_file = get_cns_file_path(fasta, fq_pair)
         yield (cns_file, fq_pair)
 
 
-def read_fastq_record(file_path):
+def read_fasta_record(file_path):
+    ''' returns the contents of a cgap fasta file '''
     with open(file_path) as input_handle:
         for record in SeqIO.parse(input_handle, 'fasta'):
             return record
@@ -21,21 +23,23 @@ def read_fastq_record(file_path):
 
 
 def yield_fasta_records(cns_path_list):
-
+    ''' yields fasta records to biopython for writing '''
     for cns_file, fq_pair in cns_path_list:
-        record = read_fastq_record(cns_file)
+        record = read_fasta_record(cns_file)
         if record:
             record.id = fq_pair
             yield record
 
 
 def write_phylip_file(phylip_file, fasta_records_gen):
+    ''' writes seq records into a phylip file '''
     with open(phylip_file, 'w+') as output_handle:
         SeqIO.write(fasta_records_gen, output_handle, 'phylip')
     return True
 
 
 def build_phylip_records(fasta, fw_fqs, rv_fqs):
+    ''' runs the phylip conversion and merge pipe '''
     phylip_file = get_phylip_file_path(fasta)
     cns_file_gen = get_cns_files_for_fasta(fasta, fw_fqs, rv_fqs)
     fasta_records_gen = yield_fasta_records(cns_file_gen)
@@ -44,6 +48,7 @@ def build_phylip_records(fasta, fw_fqs, rv_fqs):
 
 
 def build_phylip_records_argslist(args):
+    ''' runs the phylip conversion merge pipe with one arg '''
     if len(args) == 3:
         fasta, fw_fqs, rv_fqs = args
         build_phylip_records(fasta, fw_fqs, rv_fqs)
