b5e38d92f39c4b38128aacecce36d58828501ef9
Ryan Culligan <rrculligan@gmail.com>
2016-09-20 07:37:36 -0500

    add file skipping.

--- cgap/convert_to_phylip.py
+++ cgap/convert_to_phylip.py
@@ -70,7 +70,10 @@ def build_phylip_records(fasta, fw_fqs, rv_fqs):
     fasta_records_gen = list(yield_fasta_records(cns_file_gen))
     write_phylip_file(phylip_file, fasta_records_gen)
     write_codeml_phylip_file(codeml_phylip_file, fasta_records_gen)
-    write_nexus_file(nexus_file, fasta_records_gen)
+    try:
+        write_nexus_file(nexus_file, fasta_records_gen)
+    except ValueError:
+        pass
     return True
 
 
